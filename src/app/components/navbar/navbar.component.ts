da28be70d031f8011d765a22981e5a2984d5eac2
Soheila Golestani <54280446+Soheilaa@users.noreply.github.com>
2024-11-20 15:21:41 +0100

    The desktop and part of mobile version of the navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -20,14 +20,38 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 
 export class NavbarComponent {
+  isSidebarOpen = false;
   showDropdown = false;
   isNavbarOpen = false;
+  showCategories = false;
+
+  categories = [
+    { name: 'ALL COLLECTIONS', link: '/product-list', image: 'assets/all.jpg' },
+    { name: 'Bracelets', link: '/bracelets', image: 'assets/bracelets/bracelets.webp' },
+    { name: 'Rings', link: '/rings', image: 'assets/rings.webp' },
+    { name: 'Necklaces', link: '/necklaces', image: 'assets/necklaces.webp' },
+    { name: 'Earrings', link: '/earrings', image: 'assets/earrings.webp' },
+    { name: 'New In', link: '/new-in', image: 'assets/newin.webp' },
+  ];
+
+  navigateToCategory(link: string) {
+    this.showCategories = false; 
+    window.location.href = link;
+  }
+
+  toggleCategoriesDropdown() {
+    this.showCategories = !this.showCategories;
+  }
 
   toggleNavbar() {
     this.isNavbarOpen = !this.isNavbarOpen;
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
   }
 }
